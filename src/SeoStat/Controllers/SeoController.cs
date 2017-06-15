9367cca6d7907a904c0ca2d25642c804df14ce63
Sergei Sviridov <sergei.sviridov@readify.net>
2017-06-15 14:40:46 +1000

    Raw and straightforward implementation of google scrapping using WebBrowser in console app and new  process to run it

---
+++ src/SeoStat/Controllers/SeoController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace SeoStat.Controllers
+{
+    [Route("api/[controller]")]
+    public class SeoController : Controller
+    {
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            var location = AppDomain.CurrentDomain.BaseDirectory;
+            return await Task.Run(() =>
+            {
+                var proc = new System.Diagnostics.Process
+                {
+                    StartInfo = new System.Diagnostics.ProcessStartInfo
+                    {
+                        FileName = location + "\\\\WebScraper.exe",
+                        Arguments = "command line arguments to your executable",
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        CreateNoWindow = true
+                    }
+                };
+                proc.Start();
+                string res = "";
+                while (!proc.StandardOutput.EndOfStream)
+                {
+                    res = proc.StandardOutput.ReadLine();
+                    // do something with line
+                }
+                return res;
+            });
+
+        }
+
+    }
+}
---
+++ src/WebScraper/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tcs = new TaskCompletionSource<string>();
+            Thread thread = new Thread(() =>
+            {
+                WebBrowser wb = new WebBrowser();
+                wb.AllowNavigation = true;
+                wb.ScrollBarsEnabled = true;
+                wb.ScriptErrorsSuppressed = true;
+                wb.Visible = false;
+                wb.Navigate("https://www.google.com.au/#q=online+title+search&start=0");
+
+                while (wb.ReadyState != WebBrowserReadyState.Complete)
+                {
+                    Thread.Sleep(500);
+                    System.Windows.Forms.Application.DoEvents();
+                }
+
+                var el = wb.Document.GetElementById("rso");
+                while (el == null)
+                {
+                    Thread.Sleep(500);
+                    el = wb.Document.GetElementById("rso");
+                    System.Windows.Forms.Application.DoEvents();
+                }
+
+                var infoCount = el.GetElementsByTagName("h3").Cast<HtmlElement>().Where(h => h.FirstChild.GetAttribute("href").Contains("infotrack.com.au")).Count();
+
+                var dock = wb.Document;
+                tcs.SetResult(string.Empty);
+
+            });
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+            thread.Join();
+            var res = tcs.Task.Result;
+            Console.WriteLine("Done");
+            return;
+        }
+    }
+}
